b193991810f5c43fca132f835b1084f004ccf560
amccollam <andi.mccollam@gmail.com>
2016-02-15 16:51:25 -0500

    Question 3 completed, question 2.b. is a mess. Fixed some spelling errors.

--- BS720/HW3/Assignment3.R
+++ BS720/HW3/Assignment3.R
@@ -15,7 +15,7 @@ ageweight<-data.frame(age,weight)
 
 
 
-# a)	Create a new function called “newage” that creates 
+# b)	Create a new function called “newage” that creates 
 #a new variable called age_cat variable that is defined as 
 #follows:
 #   1 if age is 20 or less
@@ -43,7 +43,7 @@ newage <- function(age) {
 # ageweight dataset to create the age_cat variable. (15pts)
 
 #"ordinary assignments done within the function are local 
-#and remporary adn are lost after exit from the function" 
+#and temporary and are lost after exit from the function" 
 #(Venables,Smith,2015) 
 #That is, age_cat must be assigned outside of the function 
 #to become part of the working environment.
@@ -70,9 +70,61 @@ stem(fram_heart$weight)
 stem(fram_heart$mi)
 stem(fram_heart$dia)
 
-#Discovered that height null value is actually -9
+#Discovered that height null value is actually -9, not 9
+#$height null = -9
+#$weight null = -99
+#$mi null = 999
+#$dia null = 999
+fram_heart_copy<-fram_heart
+
+#this indexes all null height values
+null_height_index<-which(fram_heart$height==-9)
+#this moves the null height rows into a new table
+null_height_fram_heart<-fram_heart[null_height_index,]
+
+for(i in ncol(null_height_index)){
+  fram_heart$height<-NA
+}
+
+
+#this isn't working
+fram_heart_testing<-ifelse(fram_heart[null_height_index,],NA,fram_heart$height)
 
 
 
 
+#3.	Create a dataset called “cardio” which only contains the 
+# subset of participant from the fram_heart dataset who had any 
+# of the cardiovascular risk factors (smoke, hypertension, and 
+# hypercholesterolemia).  (Hint: You can either use an ifelse 
+# statement with multiple conditions OR you can first use the 
+# apply statement to sum the smoke, hypertension and 
+# hypercholesterolemia variables and then take only those where 
+# the sum is greater than 1). (20pts)
+
+#Relevant columns (4, 5, 6):
+fram_heart$smoke
+fram_heart$htn
+fram_heart$hca
+
+#verified fram_heart data structure
+str(fram_heart)
+
+cv_risk<-sum(fram_heart$smoke,
+             fram_heart$htn,
+             fram_heart$hca)
+
+cv_risk<-apply(fram_heart[,4:6],sum)
+
+#this returned a single value 0
+cardio_risk<-ifelse(fram_heart$smoke==0 && fram_heart$htn==0 && fram_heart$hca==0,"0","1")
+#this returned an array. Adding as.integer got rid of ""
+cardio_risk_ifelse<-as.integer(ifelse(fram_heart$smoke==0 & fram_heart$htn==0 & fram_heart$hca==0,"0","1"))
+
+
+
+#this one worked
+cardio_risk<-apply(fram_heart[,c(4:6)],1,sum)
+has_cardio_risk<-which(cardio_risk>0)
+cardio<-fram_heart[has_cardio_risk,]
 
