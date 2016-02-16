56c1e9746abc7fac8a78acd4ce6e0cf10b6bc8bc
amccollam <andi.mccollam@gmail.com>
2016-02-16 14:13:57 -0500

    Initial commit

---
+++ BS720/HW4/Assignment4.R
@@ -0,0 +1,6 @@
+#Homework 4
+#Due February 22, 2016
+
+# 1.	Conduct the appropriate test (depending on whether or 
+# not the assumptions are met) to see if the average nitrogen 
+# oxide is equal to 0.5.
