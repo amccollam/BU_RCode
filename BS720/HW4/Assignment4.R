f6cad8f534bac02b923715def68660858943352b
amccollam <andi.mccollam@gmail.com>
2016-02-16 16:35:50 -0500

    Question 2 complete

--- BS720/HW4/Assignment4.R
+++ BS720/HW4/Assignment4.R
@@ -30,3 +30,39 @@ shapiro.test(nox)
 # test instead to test if the average nitrogen oxide is equal to 0.5
 
 wilcox.test(nox, mu=0.5)
+
+
+ 
+# 2.	 Conduct the appropriate test (depending on whether or not the 
+# assumptions are met) to see if the crime rate is the same for houses 
+# in which the tract bounds the Charles River versus houses which do not.
+#chas = Charles River dummy variable (1 if tract bounds river, 0 otherwise)
+#crim = Per capita crime rate by town
+
+#to check for normal distribution, first check a pair of histograms
+par(mfrow=c(1,2))
+hist(crim[which(chas=="1")],main="Bounds Charles",xlab="Crime Rate")
+hist(crim[which(chas=="0")],main="Does Not Bound",xlab="Crime Rate")
+
+#to check assumption of equal variance
+var.test(crim~chas)
+
+#Since assumptions not met, must use Wilcoxon nonparametric Rank Sum Test 
+#to compare Medians
+wilcox.test(crim~chas)
+#Fail to reject H0 that means are the same.
+
+#just for funsies running the t-test despite violating assumptions
+t.test(crim~chas,var=T)
+
+# Two Sample t-test
+# 
+# data:  crim by chas
+# t = 1.2567, df = 504, p-value = 0.2094
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -1.066261  4.851815
+# sample estimates:
+#   mean in group 0 mean in group 1 
+# 3.744447        1.851670 
+
