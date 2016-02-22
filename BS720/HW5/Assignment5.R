62d73116c8aac34edd8efcf16779fe2472901a02
amccollam <andi.mccollam@gmail.com>
2016-02-22 15:34:59 -0500

    There was a bug, think it's gone now

---
+++ BS720/HW5/Assignment5.R
@@ -0,0 +1,17 @@
+#1.	Use the R dataset airquality to assess the relationship between 
+#the Ozone and the Temp variables. 
+
+# a) Use a histogram to assess normality of the Ozone and the Temp 
+# variables. Are the two variables normally distributed? Output the 
+# histogram as a .png file and include it in your homework. You do 
+# not need to perform a formal test of normality for this question. 
+
+head(airquality)
+str(airquality)
+attach(airquality)
+
+png(filename="OzoneTemp.png")
+par(mfrow=c(2,1));
+hist(Ozone, main="Mean Ozone at Roosevelt Island", xlab="Ozone ppb");
+hist(Temp, main="Maximum Daily Temperature at La Guardia Airpot", xlab="Temp F")
+dev.off()
