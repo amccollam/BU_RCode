2b4490e50419c3454912c5eb1e77ee117c949c6b
amccollam <andi.mccollam@gmail.com>
2016-02-22 15:52:14 -0500

    Question 1, a&b

--- BS720/HW5/Assignment5.R
+++ BS720/HW5/Assignment5.R
@@ -15,3 +15,13 @@ par(mfrow=c(2,1));
 hist(Ozone, main="Mean Ozone at Roosevelt Island", xlab="Ozone ppb");
 hist(Temp, main="Maximum Daily Temperature at La Guardia Airpot", xlab="Temp F")
 dev.off()
+
+# b) Use a scatterplot to assess the linearity assumption between the 
+# Ozone and the Temp variables. Do the two variables appear to have a 
+# linear trend? Output the scatterplot as a .png file and include it 
+# in your homework. 
+
+
+dev.off()
+png(filename="OzoneTemp_scatter.png")
+plot(Ozone~Temp, main="Ozone Against Temp",ylab="Ozone (ppb)",xlab="Temp (F)")
