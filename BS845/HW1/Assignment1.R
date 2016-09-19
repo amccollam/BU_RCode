e974994aa800655a7ae2114d026e1e6e44708ae8
amccollam <andi.mccollam@gmail.com>
2016-09-19 10:55:21 -0400

    HW questions

---
+++ BS845/HW1/Assignment1.R
@@ -0,0 +1,11 @@
+# Please submit R codes and outputs for each problem.
+
+# 1.	Read the Framingham Heart Study Teaching Dataset (fram.csv) into R. 
+# a.	What is the total number of rows and of columns of this dataset?
+# b.	Count the total number of non-missing values in total cholesterol (TOTCHOL).
+# c.	Compute the mean, minimum, maximum and standard deviation of TOTCHOL.
+# d.	Participants are identified by their randomized ID numbers (RANDID). What is the total number of participants in this dataset? What is the average number of observations for a subject?
+# e.	Select participants who were diabetic (DIABETES), hypertensive (HYPERTEN), had CVD (CVD), with BMI 27 or higher and aged 50 or younger;  export data for these participants with following variables to a coma (or tab) delimited plain text file, with variable names in the first line in such order:
+  # RANDID, TOTCHOL, AGE, SEX, BMI, GLUCOSE, DIABETES, SYSBP, DIABP, HYPERTEN, CVD. 
+# f.	Select observations for participants with RANDID being the following 
+# 9771959, 6461117, 5422551, 3026761, 4696586, 7414089, 2024096, 7561936, 3541454, 4409179
