2709f41d059cd2f8959fa7373d3f1add24da49ab
amccollam <andi.mccollam@gmail.com>
2016-09-24 10:32:27 -0400

    Initial commit

---
+++ BS845/HW3/HW3.R
@@ -0,0 +1,19 @@
+Homework 3 
+Please submit your R codes and output, and additional answers to the questions.
+For all figures, please properly label x and y axes and provide a title to the figure.
+If there are multiple plotting symbols or different colors in a figure, provide a legend to annotate the figure.
+Using the Framingham Heart Study teaching dataset,
+1.	Using R internal dataset, state.x77, generate scatter plots with smoothed lines to examine the relationship between Income and Illiterary, and between Income and Life Exp respectively. Place the two plots side by side in one figure.
+Comment on the linearity of the relationship between the plotted variables.
+
+2.	Check if the distribution of TOTCHOL at PERIOD equal to 1 is normal distributed. Please comment on the normality of TOTCHOL 
+a.	If you feel the distribution if not normal, apply an appropriate transformation on TOTALCHOL to make it approximately normal. 
+b.	Show that the transformed variable is approximately normal using normal QQ plot and histogram with normal density curve and a kernel density curve in the same plot. Please plot the two figures in a 1x2 array.
+
+3.	Titanic is an R internal dataset. Use barplot to examine the difference in survival rate for different group of people.
+a.	Reduce the Titanic into a two-way table with Class as row variable, and other factors and outcome combinations as column variable.
+b.	Compute survival rate for each Age and Sex combination within each Class
+c.	Draw a bar plot with bars grouped by Class
+d.	Comment on if and how each factor affects survival rate for different factors.
+
+
