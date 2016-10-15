f2906a2972a8fae4cd99109166bac7cb7f4e9e61
amccollam <andi.mccollam@gmail.com>
2016-10-15 11:12:08 -0400

    Initial Commit

---
+++ BS845/HW6/HW6.R
@@ -0,0 +1,11 @@
+attitude is from a survey of the clerical employees of a large financial organization, 
+the data are aggregated from the questionnaires of the approximately 35 employees for 
+each of 30 (randomly selected) departments. The numbers give the percent proportion of 
+favourable responses to seven questions in each department. Please use help(attitude) 
+to view the manual of the dataset.
+
+The overall purpose is to fit a model to examine the association of rating with all the 
+other variables in the dataset. Please carry out exploratory data analyses include 
+normality check, correlation and t-tests to examine crude association. Fit a linear 
+model with model diagnosis and evaluate goodness of fit the model. Improve the model 
+based on diagnostic results. 
