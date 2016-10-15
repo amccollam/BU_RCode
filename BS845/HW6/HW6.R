01021bf797e7133c1357eb840f64bd8b1117b45c
amccollam <andi.mccollam@gmail.com>
2016-10-15 15:47:02 -0400

    Checked all variables and combinations for assumptions for linear regression.  Ready to start constructing model.

--- BS845/HW6/HW6.R
+++ BS845/HW6/HW6.R
@@ -10,4 +10,59 @@ normality check, correlation and t-tests to examine crude association. Fit a lin
 model with model diagnosis and evaluate goodness of fit the model. Improve the model 
 based on diagnostic results. 
 
-ljhklj
+
+
+attitude;
+str(attitude);
+summary(attitude);
+
+#comparing all variables to all other variables using scatterplots
+plot(attitude,panel=panel.smooth);
+
+#Evaluating rating as a response varaible
+par(mfrow=c(1,2));
+hist(attitude$rating);
+qqnorm(attitude$rating);
+qqline(attitude$rating)
+#The response variable does not appear to be perfectly normally distributed
+
+#generating more graphs about response variable
+par(mfrow=c(2,2))
+plot(attitude$rating)
+hist(attitude$rating, main= "Rating")
+qqnorm(attitude$rating, main = "QQ-plot for Rating")
+qqline(attitude$rating)
+boxplot(attitude$rating, main="Rating")
+
+#checking normality of response formally using Shapiro test of normality
+shapiro.test(attitude$rating)
+
+
+#doing quick simple linear regressions to identify important predictor variables
+attach(attitude)
+LMComplaints<-lm(rating~complaints);
+LMPrivileges<-lm(rating~privileges);
+LMLearning<-lm(rating~learning);
+LMRaises<-lm(rating~raises);
+LMCritical<-lm(rating~critical);
+LMAdvance<-lm(rating~advance);
+
+plot(LMComplaints,main="Complaints");
+plot(LMPrivileges,main="Privileges");
+plot(LMLearning,main="Learning");
+plot(LMRaises,main="Raises");
+plot(LMCritical,main="Critical");
+plot(LMAdvance,main="Advance");
+
+summary(LMComplaints);
+summary(LMPrivileges);
+summary(LMLearning);
+summary(LMRaises);
+summary(LMCritical);
+summary(LMAdvance);
+
+
+
+
+
+
