51381e7bfe6b9936618c896f3526993f6696f1e7
amccollam <andi.mccollam@gmail.com>
2016-12-09 19:53:52 -0500

    Taking a look at the data with subsetted columns.

--- BS845/FinalProject/FinalProject.R
+++ BS845/FinalProject/FinalProject.R
@@ -3,3 +3,14 @@ Chronic_Conditions_PUF_2010 <- read.csv("~/Dropbox/2016_Fall/BS845/Project/2010_
 attach(Chronic_Conditions_PUF_2010)
 #Creating a subset of the original Medicare data to only show demographics, comorbidities, and full year counts.
 WholeYearCC_2010<-Chronic_Conditions_PUF_2010[,c(1:15,24:30,39:45,48,53:55)]
+detach(Chronic_Conditions_PUF_2010);
+attach(WholeYearCC_2010)
+str(WholeYearCC_2010)
+summary(WholeYearCC_2010)
+#BENE_COUNT_PD_EQ_12, AVE_PDE_CST_PD_EQ_12, AVE_PDE_PD_EQ_12 all have 4537 NA's.
+#What are these rows like?  Can they be omitted?
+PDNA<-subset(WholeYearCC_2010,is.na(AVE_PDE_PD_EQ_12))
+summary(PDNA)
+
+#NA rows are for people who don't have Part D at all it appears (or Part C as it happens)
+#This hits both genders, and all age categories
