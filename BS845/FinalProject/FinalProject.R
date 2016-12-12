6b2d76c55a4c5130d8abf6682ca81b7cd7994702
amccollam <andi.mccollam@gmail.com>
2016-12-12 14:17:09 -0500

    Level of frustration plus help from prof

--- BS845/FinalProject/FinalProject.R
+++ BS845/FinalProject/FinalProject.R
@@ -7,12 +7,13 @@ detach(Chronic_Conditions_PUF_2010);
 attach(WholeYearCC_2010)
 str(WholeYearCC_2010)
 summary(WholeYearCC_2010)
+
 #BENE_COUNT_PD_EQ_12, AVE_PDE_CST_PD_EQ_12, AVE_PDE_PD_EQ_12 all have 4537 NA's.
-#What are these rows like?  Can they be omitted?
+#What are these rows like?  Can/should they be omitted?
 PDNA<-subset(WholeYearCC_2010,is.na(AVE_PDE_PD_EQ_12))
 summary(PDNA)
 
-#NA rows are for people who don't have Part D at all it appears (or Part C as it happens)
+#These NA rows are for people who don't have Part D at all it appears (or Part C as it happens)
 #This hits both genders, and all age categories
 #Need to remove NA from WholeYearCC_2010
 WholeYearCC_2010_WD<-subset(WholeYearCC_2010,AVE_PDE_PD_EQ_12>0)
@@ -35,13 +36,16 @@ summary(PDP_2010)
 rm(ComorbidNA,PDNA,WholeYearCC_2010_WD2,WholeYearCC_2010_WD)
 detach()
 
+#Saving this file to my project folder to keep a version untouched.
+str(PDP_2010)
+attach(PDP_2010)
+write.table(PDP_2010, file="PDP_2010.csv",quote=F, sep=",", na="", row.names=F)
+
 hist(PDP_2010$AVE_PDE_PD_EQ_12, main="Average Prescriptions Filled per Beneficiary, 2010\nFull Year Part D Only", xlab="Average Prescriptions by Category")
 
 
 #let's run a quick and dirty poisson to see what happens:
-str(PDP_2010)
-attach(PDP_2010)
-write.table(PDP_2010, file="PDP_2010.csv",quote=F, sep=",", na="", row.names=F)
+
 modp1<-glm(AVE_PDE_PD_EQ_12~., family=poisson, data=PDP_2010)
 summary(modp1)
 #Hm.  glm isn't working with poisson because AVE_PDE_PD_EQ_12 is an average, not an integer.
@@ -79,6 +83,29 @@ summary(modp2)
 drop1(modp2,test="Chisq")
 
 #This is still a terrible model.  Can I do forward stepwise?
+#She used stepAIC in class, part of MASS package.
+
+stepAIC(glm(AVE_PDE_PD_EQ_12~AVE_PDE_PD_EQ_12~BENE_SEX_IDENT_CD+BENE_AGE_CAT_CD
+            +CC_ALZHDMTA+CC_CANCER+CC_CHF+CC_CHRNKIDN+CC_COPD+CC_DEPRESSN+CC_DIABETES+CC_ISCHMCHT+CC_OSTEOPRS+CC_RA_OA+CC_STRKETIA
+            +CC_2_OR_MORE+DUAL_STUS+BENE_COUNT_PA_EQ_12+AVE_IP_ADM_PA_EQ_12+AVE_SNF_DAYS_PA_EQ_12
+            +BENE_COUNT_PB_EQ_12+AVE_CA_VST_PB_EQ_12+AVE_OP_VST_PB_EQ_12+BENE_COUNT_PC_EQ_12+BENE_COUNT_PD_EQ_12+AVE_PDE_CST_PD_EQ_12
+            , family=poisson, data=PDP_2010),direction = "both")
 
 
+#AIC=Inf happens if using Poisson with non-integer values.
 
+#Does Binomial work?
+modp1<-glm(AVE_PDE_PD_EQ_12~., family=binomial, data=PDP_2010);
+summary(modp1)
+#No, because output needs to be between 0-1
+
+#making matrix of scatter plots to bring to meeting (this will be ugly)
+plot(PDP_2010,panel=panel.smooth)
+#data too big - this would need to be broken out.  Not even sure if it's necessary...
+
+#Rounded total RX col added
+PDP_2010_WD$TotRx<- (BENE_COUNT_PD_EQ_12 * AVE_PDE_PD_EQ_12)
+PDP_2010_WD$TotRxRound<- round(PDP_2010_WD$TotRx, digits=0)
+
+#model built with prof
+mod_prof<-glm(TotRxRound~BENE_COUNT_PD_EQ_12+CC_ALZHDMTA+CC_CANCER+CC_CHF+CC_COPD, family=poisson, data=PDP_2010_WD)
